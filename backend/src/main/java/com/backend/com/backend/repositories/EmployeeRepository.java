58219660c49a1d2b7ed841f70235a2dcd1fd97b4
josimarrenepont <jnrenepont@hotmail.com>
2023-09-14 19:59:09 -0300

    Created Repository-Employee/Separation/User

---
+++ backend/src/main/java/com/backend/com/backend/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.backend.com.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.com.backend.entities.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ backend/src/main/java/com/backend/com/backend/repositories/SeparationRepository.java
@@ -0,0 +1,9 @@
+package com.backend.com.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.com.backend.entities.Separation;
+
+public interface SeparationRepository extends JpaRepository<Separation, Long> {
+
+}
