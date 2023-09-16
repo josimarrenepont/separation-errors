76e7e43057d7ad691d7537a04a67d4de692c090e
josimarrenepont <jnrenepont@hotmail.com>
2023-09-15 23:06:19 -0300

    findById Controller-EMployee/Separation/User

--- backend/src/main/java/com/backend/com/backend/controllers/EmployeeController.java
+++ backend/src/main/java/com/backend/com/backend/controllers/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,13 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> findAll() {
         List<Employee> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Long id) {
+        Employee obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
     }
 
 }
