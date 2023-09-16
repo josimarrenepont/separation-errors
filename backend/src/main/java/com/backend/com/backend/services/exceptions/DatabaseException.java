c026333ad651b01342504190aa16cea557eadbd5
josimarrenepont <jnrenepont@hotmail.com>
2023-09-15 23:38:10 -0300

    Created class DatabaseException

---
+++ backend/src/main/java/com/backend/com/backend/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.backend.com.backend.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super("message");
+    }
+
+}
