94c89b69ed82d0d313abd3f41c636c450be1f7c0
josimarrenepont <jnrenepont@hotmail.com>
2023-09-16 00:14:02 -0300

    Employee delete

--- backend/src/main/java/com/backend/com/backend/services/EmployeeService.java
+++ backend/src/main/java/com/backend/com/backend/services/EmployeeService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.backend.com.backend.entities.Employee;
 import com.backend.com.backend.repositories.EmployeeRepository;
+import com.backend.com.backend.services.exceptions.DatabaseException;
 import com.backend.com.backend.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -23,6 +26,20 @@ public class EmployeeService {
     public Employee findById(Long id) {
         Optional<Employee> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Employee insert(Employee obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
 }
