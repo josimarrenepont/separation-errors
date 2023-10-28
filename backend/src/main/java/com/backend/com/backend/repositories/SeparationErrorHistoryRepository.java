7efc732ff8de4d8ade053e95acf42104f77bfa8a
josimarrenepont <jnrenepont@hotmail.com>
2023-10-27 22:55:49 -0300

    Create Class SeparationErrorHistoryRepository

---
+++ backend/src/main/java/com/backend/com/backend/repositories/SeparationErrorHistoryRepository.java
@@ -0,0 +1,8 @@
+package com.backend.com.backend.repositories;
+
+import com.backend.com.backend.entities.SeparationErrorHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeparationErrorHistoryRepository extends JpaRepository<SeparationErrorHistory, Long> {
+
+}
