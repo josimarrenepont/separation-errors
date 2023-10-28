c2f74badb8d0362a78c321fa5c7a74f8aa4dc08f
josimarrenepont <jnrenepont@hotmail.com>
2023-10-27 23:06:49 -0300

    Create Class SeparationErrorHistoryService

---
+++ backend/src/main/java/com/backend/com/backend/services/SeparationErrorHistoryService.java
@@ -0,0 +1,28 @@
+package com.backend.com.backend.services;
+
+import com.backend.com.backend.entities.SeparationErrorHistory;
+import com.backend.com.backend.repositories.SeparationErrorHistoryRepository;
+import com.backend.com.backend.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SeparationErrorHistoryService {
+
+    @Autowired
+    private SeparationErrorHistoryRepository errorHistoryRepository;
+
+
+    public List<SeparationErrorHistory> findAll(){
+        return errorHistoryRepository.findAll();
+    }
+
+    public SeparationErrorHistory findById(Long id){
+        Optional<SeparationErrorHistory> obj = errorHistoryRepository.findById(id);
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+
+    }
+}
